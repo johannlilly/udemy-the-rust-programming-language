a7ea7691add07d40fce95e8394100b063240aab9
Johann Lilly 李光 يوهان ليل Джон Лилли <johannlilly@users.noreply.github.com>
2020-02-01 21:32:02 -0500

    boolean
    end of primitive types

--- exercise-files/types-and-variables/src/main.rs
+++ exercise-files/types-and-variables/src/main.rs
@@ -2,8 +2,8 @@ use std::mem;
 
 fn main()
 {
-  // non-whole numbers
-  let e = 2.5; // double-precision value, 8 bytes or 64 bits, f64
-  //  e:f32 = 2.5
-  println!("e = {}, size = {} bytes", e, mem::size_of_val(&e));
+  // true false
+  let g = false;
+  println!("e = {}, size = {} bytes", g, mem::size_of_val(&g)); // 1 byte
+  let f = 4>0; // true
 }
