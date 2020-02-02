04dc2b03d51791ade59b497c879ca3a53e010c8f
Johann Lilly 李光 يوهان ليل Джон Лилли <johannlilly@users.noreply.github.com>
2020-02-01 21:23:06 -0500

    double-precision value

--- exercise-files/types-and-variables/src/main.rs
+++ exercise-files/types-and-variables/src/main.rs
@@ -2,7 +2,8 @@ use std::mem;
 
 fn main()
 {
-  let d = 'x';
-  println!("d = {}, size = {} bytes", d, mem::size_of_val(&d));
-  
+  // non-whole numbers
+  let e = 2.5; // double-precision value, 8 bytes or 64 bits, f64
+  //  e:f32 = 2.5
+  println!("e = {}, size = {} bytes", e, mem::size_of_val(&e));
 }
